f992d1552bbb2ed5f529c4da2dbb7b754b1ed9cc
Niels Dequeker <niels.dequeker@gmail.com>
2014-10-25 19:32:11 +0200

    skeletonning

---
+++ js/app/main.js
@@ -0,0 +1,16 @@
+angular.module('sw', ['ui.bootstrap'])
+  .controller('MainCtrl', ['$scope', 'projects', '$anchorScroll', '$location', function ($scope, projects, $anchorScroll, $location) {
+    $scope.projects = projects;
+
+    $scope.viewProject = function (project) {
+      $scope.currentProject = project;
+
+      var easeDurationInMiliseconds = 750;
+      $('html, body').animate({
+        scrollTop: angular.element('.current-project').offset().top
+      }, easeDurationInMiliseconds);
+    };
+
+    // slideshow
+    $scope.interval = 5000;
+  }]);
---
+++ js/app/projects.js
@@ -0,0 +1,48 @@
+angular.module('sw')
+  .constant('projects', [
+    {
+      title: 'My First Project',
+      intro: 'Intro',
+      description: 'Learning how to sculpture',
+      pictures: [
+        {
+          src: 'images/sculpturing/day-1.jpg',
+          text: 'Picture 1 text'
+        },
+        {
+          src: 'images/sculpturing/kitten.jpg',
+          text: 'Picture 2 text'
+        }
+      ]
+    },
+    {
+      title: 'My Second Project',
+      intro: 'Intro',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt ultricies ligula, eget porta felis vestibulum quis. Nullam in nulla ipsum. Vestibulum tincidunt orci eu metus tristique, id laoreet enim pretium. Donec porttitor dolor ut felis lobortis euismod. Pellentesque suscipit ipsum eu suscipit ornare. Suspendisse tortor orci, tempus sed gravida vel, luctus non purus. Aliquam dapibus sagittis enim vel aliquam. Vivamus pharetra, magna et ullamcorper fringilla, metus nunc laoreet odio, vitae sagittis nunc justo a odio. Nunc dapibus id mi ut rhoncus. In efficitur mauris et facilisis tempus. Sed placerat molestie tempor. Donec non ligula in lorem volutpat elementum. Mauris commodo faucibus faucibus. Proin vehicula lectus mi, id tristique nisl imperdiet id.',
+      pictures: [
+        {
+          src: 'images/sculpturing/day-1.jpg',
+          text: 'Picture 1 text'
+        },
+        {
+          src: 'images/sculpturing/kitten.jpg',
+          text: 'Picture 2 text'
+        }
+      ]
+    },
+    {
+      title: 'My Third Project',
+      intro: 'Intro',
+      description: 'Sculpturing again, cause that\'s how I roll...',
+      pictures: [
+        {
+          src: 'images/sculpturing/day-1.jpg',
+          text: 'Picture 1 text'
+        },
+        {
+          src: 'images/sculpturing/kitten.jpg',
+          text: 'Picture 2 text'
+        }
+      ]
+    }
+  ]);
