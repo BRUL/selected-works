5fe4d2b3dd035fed9f4d6bc1ef6a800f9e8f0a7a
Niels Dequeker <niels.dequeker@gmail.com>
2014-10-26 16:07:20 +0100

    Add routes

--- js/app/main.js
+++ js/app/main.js
@@ -1,16 +1,33 @@
-angular.module('sw', ['ui.bootstrap', 'ngSanitize'])
-  .controller('MainCtrl', ['$scope', 'projects', '$anchorScroll', '$location', '$timeout', function ($scope, projects, $anchorScroll, $location, $timeout) {
+angular.module('sw', ['ui.bootstrap', 'ngSanitize', 'ngRoute'])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/:projectId?', {
+        controller: 'MainCtrl',
+        templateUrl: 'js/app/views/project.html'
+      });
+  }])
+
+  .controller('MainCtrl', ['$scope', 'projects', '$anchorScroll', '$location', '$timeout', '$route', '$rootScope',
+    function ($scope, projects, $anchorScroll, $location, $timeout, $route, $rootScope) {
     $scope.projects = projects;
 
-    $scope.viewProject = function (project) {
-      $scope.currentProject = project;
+    function setCurrentProject(projectId) {
+      $scope.currentProject = projects[projectId];
+    }
 
+    if ($route.current.params.projectId) {
+      setCurrentProject($route.current.params.projectId);
+    }
+    
+    $rootScope.$on('$locationChangeSuccess', function () {
       $timeout(function() {
+        var projectId = $route.current.params.projectId;
+        setCurrentProject(projectId);
+
         var easeDurationInMiliseconds = 0;
         $('html, body').animate({
           scrollTop: angular.element('.current-project').offset().top
         }, easeDurationInMiliseconds);
       }, 0);
-    };
-
+    });
   }]);
--- js/app/projects.js
+++ js/app/projects.js
@@ -1,6 +1,6 @@
 angular.module('sw')
-  .constant('projects', [
-    {
+  .constant('projects', {
+    'installation': {
       title: 'Installation',
       intro: 'Fractures of two very different settings have been put together. They don\'t fit, and they never will. We tried everything; turned around, stacked, glued, welded. Fragments will never be their original.',
       description: 'On a shabby wooden shelf bolted to the brick wall, a number of old jars and bottles were found, forgotten by their owner. Every bottle had a story to tell, every one was unique. In trying to replicate the bottles in their entireity, I failed. The castings broke, only the intrinsic details and patina would remain.',
@@ -28,7 +28,7 @@ angular.module('sw')
         },
       ]
     },
-    {
+    'art-pavillion': {
       title: 'Art Pavillion',
       intro: 'Je zult in de plannen en doorsnede twee volumes tegenkomen en een derde rasterelement op de achtergrond.',
       description: 'Je zult in de plannen en doorsnede twee volumes tegenkomen en een derde rasterelement op de achtergrond. Lees deze als gereedschappen met een functie. Het eerste, meest publieke element is het raster in de vorm van een U. Daar aan opgehangen zijn verschillende panelen met het werk *Genital Wallpaper* op gedrukt. Dit provocerende werk is het uithangbord en de doornenstruik van het plein; het vraagt de *passant* aandachtig te zijn voor zijn doornen maar biedt in dezelfde zin ook zijn zachte schaduw aan. In die schaduw treffen we het tweede element van dit apparatus aan: een roestige bak, manshoog. We ontdekken snel dat er een wand ontbreekt. De opening leidt ons recht de diepte in. Dit tweede element fungeert als koppelstuk tussen de straat en waar we aan het einde van de trap in uitkomen: een prefab kelderelement. Brutaal in zijn saaiheid houdt het voor ons het tweede kunstwerk op: *Abstraktes Bild (Courbet)*. Ophangen van kunstwerken is echter slechts een fractie van dit derde element zijn functie. Het houdt ook, door zijn ongemakkelijke relatie met de roestige bak waar we inkelderden, een geheim voor ons. Dat geheim wordt ons op het moment dat we *Abstraktes Bild* achter ons gelaten hebben onthult: *Barre de Bois rond*. Onduidelijk van boodschap, zeer duidelijk op afstand gehouden, onduidelijk achter de stalen treden, duidelijk een geheim. We gingen vanuithangbord over ontdekkingstocht naar geheim. Dat zijn de stadia waarin LUCA\'s meest opvallende en meest fragiel werken zich kunnen plaatsen. Dat zijn de manieren waarop je als buitenstaander dit werktuig kunt hanteren.',
@@ -44,7 +44,7 @@ angular.module('sw')
 		}
       ]
     },
-    {
+    'the-narrative-residence': {
       title: 'the Narrative Residence',
       intro: 'Storytelling is as old as mankind itself. Yet when we talk about it, we can only describe. That is why today I won\'t talk about the hidden stories that shaped this new environment, but rather read them to you, with you, so that in their purest form they might bring the message most vividly.',
       description: '"Tell me what you see." It\'s Maaike\'s voice, barely audible through the music. Psychedelic vibes repeat themself and I repeat them in my head and the curtains reapeats by waving and the floor repeats by shaking and the smokefilled room vibrates. I let my eyes wander, observing my surroundings. The high armchair blocks my sight left and right. Right before me is a glass hourglass-shaped object. Above it, a spotlight. One of those industrial things we use in theaters. It\'s the only visible source of light, and its shadows are hard and fragmented. Lightly swaying on a thin steel cable, anchored in the ceiling. A painted ceiling leads the way for my eyes to the single visible wall. High vertical schelves display translucent fluids. Slowly the black curtain that shut us off from the outer world turns smokey gray. William, our sound technician, lits a second cigar. Maaike is talking to him but I can\'t understand. The unusual object, shaped like an hourglass, seems to fill the room with psychedelic tones. From the ground up, everything trembles, shakes, vibrates on the music. I am told the sound system is integrated in the void under our feet. Along with the anti-rhythm I plunge backwards into the soft black leather that holds the body I left behind.',
@@ -60,7 +60,7 @@ angular.module('sw')
         }
       ]
     },
-    {
+    'lavievert': {
       title: 'Lavievert',
       intro: 'Lavievert is een bureau voor landschapsarchitectuur. Hun hoofdprincipe, de dialoog tussen interieur en exterieur, komt in hun hoofdkantoor op verschillende vlakken terug.',
       description: 'Lavievert is een bureau voor landschapsarchitectuur. Hun hoofdprincipe, de dialoog tussen interieur en exterieur, komt in hun hoofdkantoor op verschillende vlakken terug. Gelegen aan een wandelpark, vormen de 75 meter trappen, hellingen en passerelles een aangename interieure promenade. Vanaf de voordeur kijk je de massieve bergflank op. Bovenaan laat de voorgevel je door zijn openingen de bomen zien. Het hele gebouw wordt verdeeld in enerzijds de functionele berg en anderzijds een sterke buitenatmosfeer, ononderbroken door de gevel.',
@@ -76,7 +76,7 @@ angular.module('sw')
         }
       ]
     },
-    {
+    'on-food': {
       title: 'On Food',
       intro: 'Based on the upcoming concept of [Soylent](www.soylent.me), I explored a post-food future. The answer is a complex of restaurants and kitchens for respectively avant-garde dining and experimental cooking.',
       description: '[The Shop](http://www.de-shop.be/) is a historical site in a former harbor area of Antwerp. It was built to assign labor to harbor workers. After decades of dilapidation, only the bare structure and the facade are left. Based on the upcoming concept of [Soylent](www.soylent.me), I explored a post-food future. The answer is a complex of restaurants and kitchens for respectively avant-garde dining and experimental cooking. Central is a immense open space. To break this into separate environments, parallel walls of brick are placed in a staggered composition with kitchens in between. Above each kitchen is a restaurant, each with its own stairs.',
@@ -98,7 +98,7 @@ angular.module('sw')
         }
       ]
     },
-    {
+    'object': {
       title: 'Object',
       intro: '',
       description: '',
@@ -117,7 +117,7 @@ angular.module('sw')
         }
       ]
     },
-    {
+    'reality': {
       title: 'Reality',
       intro: '',
       description: '',
@@ -136,7 +136,7 @@ angular.module('sw')
         }
       ]
     },
-    {
+    'public-private': {
       title: 'Public-Private',
       intro: 'In search of a graphical language for the architectural public-private Thing.',
       description: '',
@@ -167,4 +167,4 @@ angular.module('sw')
         }
       ]
     }
-  ]);
+  });
