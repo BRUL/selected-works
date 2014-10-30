9999316b162df39076bd0037e3a7b08d7e9220e6
BRUL <jonathan.dequeker@gmail.com>
2014-10-30 14:01:57 +0100

    Project updates + intro before description

--- js/app/projects.js
+++ js/app/projects.js
@@ -8,39 +8,30 @@ angular.module('sw')
         {
           src: 'images/installation/overview.jpg',
         },
-        {
-          src: 'images/installation/ice.jpg',
-        },
-        {
-          src: 'images/installation/bottle-1.jpg',
-        },
-        {
-          src: 'images/installation/bottle-2.jpg',
-        },
-        {
-          src: 'images/installation/bottle-3.jpg',
-        },
-        {
-          src: 'images/installation/bottle-4.jpg',
-        },
-        {
+		{
           src: 'images/installation/mirror-images.jpg',
         },
+		{
+          src: 'images/installation/bottles.jpg',
+        },
+        {
+          src: 'images/installation/ice.jpg',
+        }
       ]
     },
     'art-pavillion': {
       title: 'Art Pavillion',
       intro: 'In the plans and section, we will notice two volumes and a third grid-like element. These should be read as tools, with a function.',
-      description: 'In the plans and section, we will notice two volumes and a third grid-like element. These should be read as tools, with a function.</p><p>The first, most public, element is the U-shaped vertical grid. Attached are several panels depicting the work <em>Genital Wallpaper</em>. This provocating work is both the signboard and the thorn of the public square; it implores the pedestrian\'s attention (the thorn) but at the same time it offers its soft shadow.</p><p>In that shadow arises the second element of our apparatus: a rusty box, one person high. Soon we notice one side is missing, leading us in and down into the depth of the street. This second element is a coupler between street and, at the end of stairs, a prefabricated concrete basement. It is the third and last of the tools.</p><p>Bold in its boringness it holds our second work of art: <em>Abstraktes Bild (Courbet)</em>. Holding art is but a fraction of this element\'s function. At the same time it holds, through its uneasy relation to the rusty box through wich we plunged down, a sectret. That secret is uncovered on the moment we have left <em>Abstraktes Bild</em> behind: <em>Barre de Bois rond</em>. Obscure in its message, clearly kept at a distance, obscure behind the steel steps, clearly a secret.</p><p>We went from signboard over exploration to secret. These are the stadia where both the most striking and most fragile works of art can be installed. These are the ways to wield this tool.',
+      description: 'The first, most public, element is the U-shaped vertical grid. Attached are several panels depicting the work <em>Genital Wallpaper</em>. This provocating work is both the signboard and the thorn of the public square; it implores the pedestrian\'s attention (the thorn) but at the same time it offers its soft shadow.<br>In that shadow arises the second element of our apparatus: a rusty box, one person high. Soon we notice one side is missing, leading us in and down into the depth of the street. This second element is a coupler between street and, at the end of stairs, a prefabricated concrete basement. It is the third and last of the tools.<br>Bold in its boringness it holds our second work of art: <em>Abstraktes Bild (Courbet)</em>. Holding art is but a fraction of this element\'s function. At the same time it holds, through its uneasy relation to the rusty box through wich we plunged down, a sectret. That secret is uncovered on the moment we have left <em>Abstraktes Bild</em> behind: <em>Barre de Bois rond</em>. Obscure in its message, clearly kept at a distance, obscure behind the steel steps, clearly a secret.<br>We went from signboard over exploration to secret. These are the stadia where both the most striking and most fragile works of art can be installed. These are the ways to wield this tool.',
       pictures: [
         {
-          src: 'images/art-pavillion/MM-exterior.jpg',
+          src: 'images/art-pavillion/MM-plan.jpg',
         },
         {
-          src: 'images/art-pavillion/MM-section.jpg',
+          src: 'images/art-pavillion/MM-exterior.jpg',
 		},
         {
-          src: 'images/art-pavillion/MM-plan.jpg',
+          src: 'images/art-pavillion/MM-section.jpg',
 		}
       ]
     },
@@ -50,29 +41,23 @@ angular.module('sw')
       description: '"Tell me what you see." It\'s Maaike\'s voice, barely audible through the music. Psychedelic vibes repeat themself and I repeat them in my head and the curtains reapeats by waving and the floor repeats by shaking and the smokefilled room vibrates. I let my eyes wander, observing my surroundings. The high armchair blocks my sight left and right. Right before me is a glass hourglass-shaped object. Above it, a spotlight. One of those industrial things we use in theaters. It\'s the only visible source of light, and its shadows are hard and fragmented. Lightly swaying on a thin steel cable, anchored in the ceiling. A painted ceiling leads the way for my eyes to the single visible wall. High vertical schelves display translucent fluids. Slowly the black curtain that shut us off from the outer world turns smokey gray. William, our sound technician, lits a second cigar. Maaike is talking to him but I can\'t understand. The unusual object, shaped like an hourglass, seems to fill the room with psychedelic tones. From the ground up, everything trembles, shakes, vibrates on the music. I am told the sound system is integrated in the void under our feet. Along with the anti-rhythm I plunge backwards into the soft black leather that holds the body I left behind.',
       pictures: [
         {
-          src: 'images/narrative-residence/BS-lobby.png',
-        },
-        {
           src: 'images/narrative-residence/BS-lounge.jpg',
         },
-        {
-          src: 'images/narrative-residence/BS-plans.jpg',
+		{
+          src: 'images/narrative-residence/BS-lobby.png',
         }
       ]
     },
     'lavievert': {
       title: 'Lavievert',
-      intro: 'Lavievert is een bureau voor landschapsarchitectuur. Hun hoofdprincipe, de dialoog tussen interieur en exterieur, komt in hun hoofdkantoor op verschillende vlakken terug.',
-      description: 'Lavievert is een bureau voor landschapsarchitectuur. Hun hoofdprincipe, de dialoog tussen interieur en exterieur, komt in hun hoofdkantoor op verschillende vlakken terug. Gelegen aan een wandelpark, vormen de 75 meter trappen, hellingen en passerelles een aangename interieure promenade. Vanaf de voordeur kijk je de massieve bergflank op. Bovenaan laat de voorgevel je door zijn openingen de bomen zien. Het hele gebouw wordt verdeeld in enerzijds de functionele berg en anderzijds een sterke buitenatmosfeer, ononderbroken door de gevel.',
+      intro: 'Lavievert is a bureau for landscape architecture. Their main principle, the dialogue between interior and exterior, recurs in the design of the main office.',
+      description: 'Situated near a public park, the 75 meter long stairs, slopes and passerelles make for a pleasant interior promenade. Viewed from the entrance door, the promenade looks like a massive hillside. On the summit, the facade lets you see through its apertures the trees of the park.<br>The entire building is divided in on the one hand a functional hillside an on the other hand a strong exterior-like atmosphere, undisturbed by the facade.',
       pictures: [
-        {
-          src: 'images/lavievert/BS-render.png',
-        },
         {
           src: 'images/lavievert/BS-section.jpg',
         },
-        {
-          src: 'images/lavievert/BS-plans.jpg',
+		{
+          src: 'images/lavievert/BS-render.png',
         }
       ]
     },
@@ -82,32 +67,26 @@ angular.module('sw')
       description: '<a href="http://www.de-shop.be">The Shop</a> is a historical site in a former harbor area of Antwerp. It was built to assign labor to harbor workers. After decades of dilapidation, only the bare structure and the facade are left. Based on the upcoming concept of <a href="http://www.soylent.me">Soylent</a>, I explored a post-food future. The answer is a complex of restaurants and kitchens for respectively avant-garde dining and experimental cooking. Central is a immense open space. To break this into separate environments, parallel walls of brick are placed in a staggered composition with kitchens in between. Above each kitchen is a restaurant, each with its own stairs.',
       pictures: [
         {
-          src: 'images/on-food/IM-model.jpg',
-        },
-        {
-          src: 'images/on-food/IM-diagram.jpg',
-        },
-        {
           src: 'images/on-food/IM-iso.png',
         },
-        {
-          src: 'images/on-food/IM-sketch.png',
+		{
+          src: 'images/on-food/IM-diagram.jpg',
         },
-        {
+		{
           src: 'images/on-food/IM-sketch-2.png',
-        }
+        },
+		{
+          src: 'images/on-food/IM-model.jpg',
+        },
       ]
     },
     'object': {
       title: 'Object',
-      intro: '',
-      description: '',
+      intro: 'Tallow+Yarn, Rubber, Wood',
+      description: '<strong>Tallow:</strong> a hard fatty substance made from rendered animal fat, used (especially formerly) in making candles and soap.<br><strong>+ Yarn:</strong> a continuous often plied strand composed of either natural or man-made fibers or filaments and used in weaving and knitting to form cloth<br><br><strong>Rubber:</strong> a tough elastic polymeric substance made from the latex of a tropical plant or synthetically.<br><br><strong>Wood: </strong> the hard fibrous material that forms the main substance of the trunk or branches of a tree or shrub, used for fuel or timber.',
       pictures: [
         {
           src: 'images/object/object-fat.jpg',
-        },
-        {
-          src: 'images/object/object-rubber.jpg',
         },
         {
           src: 'images/object/object-rubber-2.jpg',
@@ -119,7 +98,7 @@ angular.module('sw')
     },
     'reality': {
       title: 'Reality',
-      intro: '',
+      intro: 'Furniture for friends and family.',
       description: '',
       pictures: [
 		{
@@ -133,37 +112,6 @@ angular.module('sw')
         },
         {
           src: 'images/reality/C1-plans.jpg',
-        }
-      ]
-    },
-    'public-private': {
-      title: 'Public-Private',
-      intro: 'In search of a graphical language for the architectural public-private Thing.',
-      description: '',
-      pictures: [
-		{
-          src: 'images/public-private/facade-virus.jpg',
-        },
-        {
-          src: 'images/public-private/research-1.jpg',
-        },
-        {
-          src: 'images/public-private/research-2.jpg',
-        },
-        {
-          src: 'images/public-private/research-3.jpg',
-        },
-        {
-          src: 'images/public-private/research-4.jpg',
-        },
-        {
-          src: 'images/public-private/research-5.jpg',
-        },
-        {
-          src: 'images/public-private/research-6.jpg',
-        },
-        {
-          src: 'images/public-private/research-7.jpg',
         }
       ]
     }
